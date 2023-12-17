b6c9175465bbcf3a3ba34c02ef1150d93f55a16c
williamli12 <145286022+williamli12@users.noreply.github.com>
2023-12-17 18:31:05 -0500

    Add files via upload

---
+++ info.py
@@ -0,0 +1,80 @@
+
+#This File will contain the information to be displayed in your portfolio
+
+#CHANGE BELOW
+profile_picture = "images/profile.jpeg"
+about_me = "My name is William Li. I am a student at the Georgia Institute of Technology, majoring in Computer Science. "
+
+
+#CHANGE BELOW (OPTIONAL)
+linkedin_image_url = "https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
+github_image_url = "https://cdn-icons-png.flaticon.com/256/25/25231.png"
+email_image_url = "https://logowik.com/content/uploads/images/513_email.jpg"
+
+#CHANGE BELOW
+my_linkedin_url = "https://www.linkedin.com/in/william-li-b0142b290/"
+my_github_url = "https://github.gatech.edu/wli696"
+my_email_address = "liwilliam12@gmail.com"
+
+
+education_data ={
+    'Degree': 'Bachelor of Science in Computer Science',
+    'Institution': 'Georgia Institute of Technology',
+    'Location': 'Atlanta, GA',
+    'Graduation Date': '2026',
+    'GPA': '4.0'
+}
+course_data = {
+    "code":["CS 1301", "CS 1331", "MATH 1554", "CS 2050", "MATH 1552", "PHYS 2211"], 
+    "names":["Intro to CS", "Intro to OOP", "Linear Algebra", "Discrete Mathematics", "Integral Calculus", "Intro to Physics"], 
+    "semester_taken":["1st", "2nd", "2nd", "2nd", "2nd", "2nd"],
+    "skills":["The class codes in Python and some HTML/CSS/JS", "OOP(S!) (Java Edition)", "Basically row reduction", "If only my grade was discrete too...", "Integration and Series", "Classical Mechanics, Waves, and Oscillation"],
+    }
+experience_data = {
+    "Manager at China Panda": (["- Facilitated Fiancial Logistics and Transactions",
+                                                                          "- Increased Restaurant Online Advertisment", "- Integrated Online Applications (POS Systems) and Services"],"images/cook.jpg"),
+    "Tutor":(["- Tutored Elementary Students post-Covid Lockdown",
+                                                           "- Teached Missing Subjects to High Schoolers", "- Helped High Schoolers Apply for College"],"images/tutor.jpg"),
+    
+
+}
+
+projects_data = {
+    "Weather App": "I created a real-time weather app based on the user's latitue and longitude inputs.",
+    "Wolfenstein 3D": "Rendering visuals from two-dimensional images",
+    "RASAERO Backend": "Creating a file template for Georgia Tech Experimental Rocketry club"
+}
+
+programming_data = {
+    "Python": 80,
+    "HTML": 60,
+    "CSS": 60,
+}
+
+#CHANGE BELOW (OPTIONAL)
+programming_icons = {
+    "Python": "🐍",
+    "JavaScript": "☕",
+    "C": "🔍",
+}
+spoken_icons = {"Mandarin": "🇫🇷",
+    "English": "🇬🇧",
+    "Spanish":"🇪🇸"
+}
+
+#CHANGE BELOW
+spoken_data = {
+    "English": "Fluent",
+    "Mandarin": "Home-Fluent",
+    "Spanish": "Work-Fluent",
+}
+leadership_data = {
+    "President of the National Spanish Honor Society": (["- Partnered with an international organization to fund Venezuluen villages"],"images/puff.jpg"),
+    "President of the National Honor Society": (["- Established an after-school tutoring program"], "images/tutorNHS.jpg")
+}
+activity_data={
+    "GT Running Wreck": ["- Go on scheduled runs and workouts", 
+            "- Run 5ks on weekends"],
+    "Music": ["- Learning to play the piano",
+              "- Picking up the Trombone"]
+}
